f7a60fe00d1e35f6f102ed50502cdb8a4247f319
AleksSkyr <greshnik1499@gmail.com>
2023-07-16 21:24:34 +0300

    exception_0.1

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Registration.regist("ABC", "","");
+        Registration.regist("Sky", "abc","abc");
 //        System.out.print("Введите логин: ");
 //        Scanner scan = new Scanner(System.in);
 //        String login = scan.nextLine();
--- src/Registration.java
+++ src/Registration.java
@@ -1,11 +1,22 @@
 public class Registration {
 
-    private static final String CHARS = "abcdefghijklmnopqrstuywxz";
+    private static final String CHARS = "abcdefghijklmnopqrstuywxz_";
 
     public static boolean regist(String login, String password, String confirmPassword) {
-        checkLogin(login);
+
+        if (login.length() > 20) {
+            throw new WrongLoginException("Логин не должен быть больше 20 символов");
+        }
         checkAlphavit(login);
 
+        if (password.length() > 20) {
+            throw new WrongPasswordException("Пароль не должен быть больше 20 символов");
+        }
+        checkAlphavit(password);
+
+        if (password.equals(confirmPassword)) {
+            throw new WrongPasswordException("Пароли не совпадают");
+        }
 
         return true;
     }
@@ -23,17 +34,9 @@ public class Registration {
             }
             if (!found) {
                 throw new WrongLoginException("В строке недопустимые символы");
-
             }
 
         }
-    }
-
-    private static void checkLogin(String str) {
-        if (str.length() > 20) {
-            throw new WrongLoginException("Логин не должен быть больше 20 символов");
-        }
-
     }
 }
 
